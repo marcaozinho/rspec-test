b7bf626fe56157ac925426c823e91c37fbf81a34
marcaozinho <aci.marcus@gmail.com>
2016-11-22 18:12:26 -0500

    Capybara init

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  context "Testando a View User" do
+    users = User.all
+    size_old = users.size
+
+    it "Cadastrando um usuario" do
+    visit '/users/new'
+    within("#new_user") do
+      fill_in 'Name', with: 'Jackson'
+      fill_in 'Email', with: 'teste@teste.com'
+    end
+    click_button 'Create User'
+    expect(page).to have_content 'Cadastrado com sucesso!'
+
+    expect(users.size).to eql(size_old + 1)
+    end
+  end  
+
   context "Validade de um usuário" do
     it "usuario válido?" do
         user = User.new

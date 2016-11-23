979948c903861df817da01b1469efeaacaa2e55a
marcaozinho <aci.marcus@gmail.com>
2016-11-23 10:04:25 -0500

    js ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,9 +30,11 @@ class UsersController < ApplicationController
       if @user.save
         format.html { redirect_to @user, notice: 'Cadastrado com sucesso!' }
         format.json { render :show, status: :created, location: @user }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.js #views/users/create.js.erb
       end
     end
   end
